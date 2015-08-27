d8082dc42c89570cb042ec24da5243f77598a807
Subhash Chandran <subwiz@gmail.com>
2015-08-27 16:43:37 +0530

    initial import

---
+++ src/main/java/org/wiztools/anyurlhttpserver/AnyUrlHttpServerMain.java
@@ -0,0 +1,73 @@
+package org.wiztools.anyurlhttpserver;
+
+import java.io.File;
+import java.io.PrintStream;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ *
+ * @author subwiz
+ */
+public class AnyUrlHttpServerMain {
+    
+    private static void printHelp(PrintStream out) {
+        out.println("Parameters: ");
+        out.println("\t-p\t: [Mandatory] port number.");
+        out.println("\t-f\t: File to serve. When not given, prints <p>Hello World!</p>");
+        out.println("\t-c\t: Response Content-Type. Default is text/html.");
+        out.println("\t-r\t: Response character encoding. Default is utf-8.");
+    }
+    
+    public static void main(String[] args) throws Exception {
+        OptionParser parser = new OptionParser("p:f:c:r:h");
+        OptionSet options = parser.parse(args);
+        if(options.has("h")) {
+            printHelp(System.out);
+            System.exit(0);
+        }
+        
+        final int port;
+        if(options.has("p")) {
+            String portStr = options.valueOf("p").toString();
+            try {
+                port = Integer.parseInt(portStr);
+            }
+            catch(NumberFormatException ex) {
+                throw new IllegalArgumentException(
+                        "Invalid port number: " + portStr, ex);
+            }
+        }
+        else {
+            port = -1;
+            System.err.println("Mandatory parameter port not given.");
+            System.exit(1);
+        }
+        
+        final File file;
+        if(options.has("f")) {
+            String fileStr = options.valueOf("f").toString();
+            file = new File(fileStr);
+        }
+        else {
+            file = null;
+        }
+        
+        AnyUrlServlet servlet = new AnyUrlServlet();
+        servlet.setFile(file);
+        
+        Server server = new Server(port);
+        server.setStopAtShutdown(true);
+        
+        // Attach the servlet:
+        ServletContextHandler ctx = new ServletContextHandler();
+        ctx.setContextPath("/");
+        server.setHandler(ctx);
+        ctx.addServlet(new ServletHolder(servlet), "/*");
+        
+        server.start();
+    }
+}
---
+++ src/main/java/org/wiztools/anyurlhttpserver/AnyUrlServlet.java
@@ -0,0 +1,73 @@
+package org.wiztools.anyurlhttpserver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.wiztools.commons.Charsets;
+import org.wiztools.commons.StreamUtil;
+
+/**
+ *
+ * @author subwiz
+ */
+public class AnyUrlServlet extends HttpServlet {
+    private String contentType = "text/html";
+    private String charset = "utf-8";
+    private File file;
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+    
+    public void setCharset(String charset) {
+        this.charset = charset;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+    
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
+    
+    private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType(contentType);
+        resp.setCharacterEncoding(charset);
+        try(OutputStream os = resp.getOutputStream();) {
+            if(file != null && file.exists() && file.canRead()) {
+                try(InputStream is = new FileInputStream(file)) {
+                    StreamUtil.copy(is, os);
+                }
+            }
+            else {
+                byte[] out = "<p>Hello World!</p>".getBytes(Charsets.UTF_8);
+                os.write(out);
+            }
+        }
+    }
+    
+}
